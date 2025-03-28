1cc9a1676c524938b58da4d1aaf905892a0cd0b1
VitorAlbara <92799928+VitorAlbara@users.noreply.github.com>
2025-03-27 21:45:17 -0300

    Add files via upload

--- Nova pasta/Nova pasta/main.py
+++ Nova pasta/Nova pasta/main.py
@@ -1,11 +1,18 @@
 import pymongo
-
 from typing import Union
-
 from fastapi import FastAPI
+import os
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+MONGODB_URI = os.getenv("MONGODB_URI")
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -16,12 +23,14 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
+
 @app.get("/aula")
-def aula():
+def aula(item: Item):
     myclient = pymongo.MongoClient("mongodb://mongodb1:27017/")
     mydb = myclient["mydatabase"]
     mycol = mydb["customers"]
 
     mydict = { "name": "John", "address": "Highway 37" }
 
-    x = mycol.insert_one(mydict)
+    x = mycol.insert_one(item)
